fe54ac7cb41008cf9ff6cb26c62b60c7afefbef2
Sreeman07 <sreeman264@gmail.com>
2026-05-14 19:31:13 +0530

    Update func1.js

--- week 1/func1.js
+++ week 1/func1.js
@@ -1,11 +1,26 @@
-//write a function that receives 3 number agrgs and return the big number
-function bignum(a,b,c){
-    if (a>b && a>c)
-        return "a is big"
-    else if(b>c)
-        return "b is big"
+// Function to find the biggest number among three numbers
+function bignum(a, b, c) {
+
+    // Check if 'a' is greater than both 'b' and 'c'
+    if (a > b && a > c)
+
+        // Return if 'a' is the biggest
+        return "a is big";
+
+    // Check if 'b' is greater than 'c'
+    else if (b > c)
+
+        // Return if 'b' is the biggest
+        return "b is big";
+
     else
-        return "c is big"
+
+        // Return if 'c' is the biggest
+        return "c is big";
 }
-let res=bignum(3,5,1)
-console.log(res)
+
+// Call the function with sample values
+let res = bignum(3, 5, 1);
+
+// Print the result
+console.log(res);
