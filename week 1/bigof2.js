a5f76374759db47ed1338e952b436b684a462dd8
Sreeman07 <sreeman264@gmail.com>
2026-05-14 19:29:38 +0530

    Update bigof2.js

--- week 1/bigof2.js
+++ week 1/bigof2.js
@@ -1,8 +1,15 @@
+// Declare two variables
 let a = 10;
 let b = 20;
 
+// Check which number is greater
 if (a > b) {
+
+    // Executes if 'a' is greater than 'b'
     console.log(a + " is bigger");
+
 } else {
+
+    // Executes if 'b' is greater than or equal to 'a'
     console.log(b + " is bigger");
 }
